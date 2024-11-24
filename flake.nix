4e28beece2e7e3deb8e80dd7adf06a830719e682
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-11-24 13:10:28 +0000

    updated: flake
    - flake.nix
      modified: home-manager.url

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
