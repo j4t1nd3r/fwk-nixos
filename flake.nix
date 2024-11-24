616f121edc6cb4bbb1c095f46ba1b5b8c2f46a13
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-11-24 13:28:26 +0000

    updated: flake
    - flake.nix
      home-manager added comments

--- flake.nix
+++ flake.nix
@@ -5,7 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
-      url = "github:nix-community/home-manager/";
+      # home-manager unpinned from fixed release
+      url = "github:nix-community/home-manager/"; # release-24.05
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
