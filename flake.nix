181298e87001df09910f814393dcb8f8eef1ad73
Jatinder Randhawa <j4t1nd3r@gmail.com>
2025-07-17 19:31:17 +0100

    Theming fixed

--- flake.nix
+++ flake.nix
@@ -1,26 +1,25 @@
-# ./flake.nix
-
 {
-  description = "nixos flake config";
+  description = "Framework-16 system + HM profile";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    nixpkgs.url          = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url   = "github:NixOS/nixos-hardware";
+
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     plasma-manager = {
       url = "github:pjones/plasma-manager";
-      inputs = {
-        home-manager.follows = "home-manager";
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.home-manager.follows = "home-manager";
+      inputs.nixpkgs.follows      = "nixpkgs";
     };
+
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
-  outputs = { 
+  outputs = inputs@{ 
     self, 
     nixpkgs, 
     nixos-hardware, 
@@ -28,18 +27,27 @@
     plasma-manager, 
     nix-vscode-extensions, 
     ... 
-  }@inputs:
+  }:
 
-  let
+    let
     system = "x86_64-linux";
-    sddmOverlay = (final: prev: { sddm = prev.sddm-qt6; });
 
-  in {
-    nixosConfigurations = {
-      fwk-nixos = nixpkgs.lib.nixosSystem {
-        system  = "x86_64-linux";
+    sddmOverlay = final: prev: { sddm = prev.sddm-qt6; };
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+      overlays = [
+        sddmOverlay
+        inputs."nix-vscode-extensions".overlays.default
+      ];
+    };
+  in
+  {
+    nixosConfigurations."fwk-nixos" =
+      nixpkgs.lib.nixosSystem {
+        inherit system;
         specialArgs = { inherit inputs system; };
-        
         modules = [
           ./nixos/configuration.nix
           nixos-hardware.nixosModules.framework-16-7040-amd
@@ -47,6 +55,12 @@
           ({ ... }: { nixpkgs.overlays = [ sddmOverlay ]; })
         ];
       };
-    };
+
+    homeConfigurations."jat" =
+      home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit inputs; };
+        modules = [ ./home-manager/home.nix ];
+      };
   };
 }
--- modules/plasma.nix
+++ modules/plasma.nix
@@ -1,14 +1,22 @@
-# ./modules/plasma.nix
+{ config, pkgs, inputs, ... }:
 
-{ ... }:
-
+let
+  ghost = ../assets/ghost_in_the_shell.jpeg;
+  plasmaMgr = inputs.plasma-manager.packages.${pkgs.system}.default;
+in
 {
+  home-manager.users.jat.xdg.configFile."autostart/set-wallpaper.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=Set custom wallpaper
+    Exec=plasma-apply-wallpaperimage ${ghost}
+    X-KDE-autostart-phase=0
+  '';
   home-manager.users.jat.programs.plasma = {
     enable = true;
     workspace = {
-      lookAndFeel              = "org.kde.breezedark.desktop";
-      wallpaper                = ../assets/ghost_in_the_shell.jpeg;
-      wallpaperFillMode        = "preserveAspectCrop";
+      lookAndFeel       = "org.kde.breezedark.desktop";
+      wallpaperFillMode = "preserveAspectCrop";
       wallpaperBackground.blur = true;
     };
   };
