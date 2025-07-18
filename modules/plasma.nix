0a00501b8ab2265c78717b093467080095f1fcfa
Jatinder Randhawa <j4t1nd3r@gmail.com>
2025-07-18 21:19:55 +0100

    fixed theming2

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -1,17 +1,31 @@
+# ./modules/plasma.nix
+
 { config, pkgs, inputs, ... }:
 
 let
-  ghost = ../assets/ghost_in_the_shell.jpeg;
-  plasmaMgr = inputs.plasma-manager.packages.${pkgs.system}.default;
+  backWp = ../assets/ghost_in_the_shell.jpeg;
+  lockWp = ../assets/gorod-siluet-art-kiberpank.jpeg;
 in
 {
+
   home-manager.users.jat.xdg.configFile."autostart/set-wallpaper.desktop".text = ''
     [Desktop Entry]
     Type=Application
     Name=Set custom wallpaper
-    Exec=plasma-apply-wallpaperimage ${ghost}
+    Exec=plasma-apply-wallpaperimage ${backWp}
     X-KDE-autostart-phase=0
   '';
+
+  home-manager.users.jat.xdg.configFile."kscreenlockerrc" = {
+    force = true;
+    text = ''
+      [Greeter][Wallpaper][org.kde.image][General]
+      Image=file://${lockWp}
+      FillMode=2        # preserveAspectCrop
+      Blur=true
+    '';
+  };
+
   home-manager.users.jat.programs.plasma = {
     enable = true;
     workspace = {
--- modules/symlink.nix
+++ modules/symlink.nix
@@ -11,15 +11,5 @@
     ".config/Code/User/settings.json".source =
       config.lib.file.mkOutOfStoreSymlink
         "${config.home.homeDirectory}/dotfiles/vscode-settings.json";
-
-    ".config/kscreenlockerrc" = {
-      force = true;
-      text = ''
-        [Greeter]
-        WallpaperPlugin=org.kde.image
-        [Greeter][Wallpaper][org.kde.image][General]
-        Image=file://${builtins.toString ../assets/gorod-siluet-art-kiberpank.jpeg}
-      '';
-    };
   };
 }
