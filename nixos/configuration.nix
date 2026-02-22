79d1cd80c4a75702e337a84974e1a43e089da00b
Jatinder Randhawa <j4t1nd3r@gmail.com>
2026-02-22 10:43:18 +0000

    updated: config.nix
    - cleanup

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,35 +4,17 @@
 { inputs, pkgs, lib, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-    inputs.home-manager.nixosModules.home-manager
-    ../modules/default.nix
-  ];
+  # --- changes frequently ---
 
   # switch over to testing channel:
   # boot.kernelPackages = pkgs.linuxPackages_testing; # 04/09/25
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  home-manager = {
-    useGlobalPkgs   = true;
-    useUserPackages = true;
-    users.jat = import ../home/home.nix;
-    sharedModules = [ inputs.plasma-manager.homeModules.plasma-manager ];
-    extraSpecialArgs = { inherit inputs; };
-  };
-
-  users.users.jat = {
-    isNormalUser = true;
-    extraGroups  = [ "wheel" "networkmanager" "lp" "scanner" ];
-  };
 
   environment.systemPackages = with pkgs; [
     kdePackages.kate
     kdePackages.kcalc
     nvd
     nix-prefetch-git
-    home-manager
     tree
     usbutils
     hplipWithPlugin
@@ -40,17 +22,43 @@
     util-linux
   ];
 
-  nixpkgs.config.allowUnfree = true;
-
   fonts.packages = with pkgs; [ nerd-fonts.meslo-lg ];
 
-  services.libinput.touchpad.disableWhileTyping = true;
+  home-manager = {
+    useGlobalPkgs    = true;
+    useUserPackages  = true;
+    users.jat        = import ../home/home.nix;
+    sharedModules    = [ inputs.plasma-manager.homeModules.plasma-manager ];
+    extraSpecialArgs = { inherit inputs; };
+  };
 
-  time.timeZone               = "Europe/London";
-  services.xserver.xkb.layout = "gb";
-  console.keyMap              = "uk";
+  # Workaround: AMDGPU DCN 3.1.4 loses precision when Plasma 6.6+ programs
+  # the display shaper LUT, causing intermittent graphical artifacts.
+  # Disabling hardware color management forces the pipeline through software.
+  environment.sessionVariables.KWIN_DRM_NO_AMS = "1";
 
-  i18n.defaultLocale = "en_GB.UTF-8";
+  # --- set once ---
+
+  users.users.jat = {
+    isNormalUser = true;
+    extraGroups  = [ "wheel" "networkmanager" "lp" "scanner" ];
+  };
+
+  networking = {
+    hostName              = "jat-fwk-nix";
+    networkmanager.enable = true;
+  };
+
+  services.desktopManager.plasma6.enable = true;
+
+  hardware.bluetooth = { enable = true; powerOnBoot = true; };
+
+  services.printing = {
+    enable  = true;
+    drivers = [ pkgs.hplipWithPlugin ];
+  };
+
+  systemd.services.cups.wantedBy = lib.mkForce [ ]; # service not required on boot
 
   security.rtkit.enable = true;
 
@@ -62,35 +70,31 @@
     jack.enable       = true;
   };
 
-  services.desktopManager.plasma6.enable = true;
+  services.libinput.touchpad.disableWhileTyping = true;
 
-  hardware.bluetooth = { enable = true; powerOnBoot = true; };
-
-    services.printing = {
-    enable  = true;
-    drivers = [ pkgs.hplipWithPlugin ];
-  };
-
-  systemd.services.cups.wantedBy = lib.mkForce [ ]; # service not required on boot
-
-  networking = {
-    hostName            = "jat-fwk-nix";
-    networkmanager.enable = true;
-  };
+  time.timeZone               = "Europe/London";
+  services.xserver.xkb.layout = "gb";
+  console.keyMap              = "uk";
+  i18n.defaultLocale          = "en_GB.UTF-8";
 
   swapDevices = [{ device = "/swapfile"; size = 32 * 1024; }];
 
   services.power-profiles-daemon.enable = true;
   services.fwupd.enable                 = true;
 
-  # Workaround: AMDGPU DCN 3.1.4 loses precision when Plasma 6.6+ programs
-  # the display shaper LUT, causing intermittent graphical artifacts.
-  # Disabling hardware color management forces the pipeline through software.
-  environment.variables.KWIN_DRM_NO_AMS = "1";
+  nixpkgs.config.allowUnfree = true;
 
-  boot.loader.systemd-boot.enable       = true;
-  boot.loader.efi.canTouchEfiVariables  = true;
-  
+  # --- static (set at install) ---
+
+  imports = [
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.home-manager
+    ../modules/default.nix
+  ];
+
+  boot.loader.systemd-boot.enable      = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  system.stateVersion = "23.11";
+  system.stateVersion = "23.11"; # do not bump
 }
